07001f1d063672d058a4ada04f79dd8e96995bac
Ngakan Gandhi <ngandhi@pmintl.net>
2025-05-21 10:44:03 +0700

    feat: complete the kinesis event handler

--- examples/kinesis-message-processor/src/event_handler.rs
+++ examples/kinesis-message-processor/src/event_handler.rs
@@ -1,7 +1,7 @@
 use aws_lambda_events::kinesis::KinesisEvent;
 use aws_lambda_events::streams::{KinesisBatchItemFailure, KinesisEventResponse};
 use lambda_runtime::{Error, LambdaEvent};
-use shared;
+use shared::{self};
 
 pub async fn function_handler(
     event: LambdaEvent<KinesisEvent>,
@@ -13,6 +13,23 @@ pub async fn function_handler(
 
         let new_message: Result<shared::NewSensorReading, shared::MessageParseError> =
             shared::InternalKinesisMessage::new(message.clone()).try_into();
+
+        if new_message.is_err() {
+            batch_item_failures.push(KinesisBatchItemFailure {
+                item_identifier: Some(kinesis_sequence_number),
+            });
+            continue;
+        }
+
+        // Business logic goes here
+        let handle_result = shared::NewSensorReadingHandler::handle(&new_message.unwrap()).await;
+
+        if handle_result.is_err() {
+            batch_item_failures.push(KinesisBatchItemFailure {
+                item_identifier: Some(kinesis_sequence_number),
+            });
+            continue;
+        }
     }
 
     Ok(KinesisEventResponse {
